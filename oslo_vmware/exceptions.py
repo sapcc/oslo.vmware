f3f216d111702aa24c3e7179c1d25d29d0b16f32
imitev <imitev@vmware.com>
2018-01-22 01:43:30 -0800

    raise proper specific vim exceptions for lease errors

--- oslo_vmware/exceptions.py
+++ oslo_vmware/exceptions.py
@@ -295,6 +295,25 @@ def get_fault_class(name):
         LOG.debug('Fault %s not matched.', name)
     return fault_class
 
+def translate_fault(localized_method_fault, excep_msg=None):
+    """Produce proper VimException subclass object based on a vmodl.LocalizedMethodFault.
+    
+    :param excep_msg: Message to set to the exception. Defaults to localizedMessage of the fault.
+    """
+    try:
+        if not excep_msg:
+            excep_msg = six.text_type(localized_method_fault.localizedMessage)
+        fault_class = get_fault_class(localized_method_fault.fault.__class__.__name__)
+        if fault_class:
+            ex = fault_class(excep_msg)
+        else:
+            ex = exceptions.VimFaultException([name], excep_msg)
+    except Exception as e:
+        LOG.debug("Unexpected exception thrown (%s) while translating fault (%s) with message: %s.",
+                  e, localized_method_fault, excep_msg)
+        ex = VimException(message=excep_msg, cause=e)
+    
+    return ex
 
 def register_fault_class(name, exception):
     fault_class = _fault_classes_registry.get(name)
