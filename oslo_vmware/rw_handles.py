7a2d359496e399068350f881c4d877b8d6182264
Johannes Kulik <johannes.kulik@sap.com>
2020-05-12 09:34:13 +0200

    fix most of pep8 errors
    The error with "file" not defined comes from running flake8 on python3,
    it seems. Upstream change I2491e65736d2c79f1134eec6371967c39873891b will
    fix this in the rocky release.
    
    Change-Id: Ib4be3f2e0e9203f83030667338a8705b0d5be7d8

--- oslo_vmware/rw_handles.py
+++ oslo_vmware/rw_handles.py
@@ -33,9 +33,9 @@ import six.moves.urllib.parse as urlparse
 from urllib3 import connection as httplib
 
 from oslo_vmware._i18n import _
+from oslo_vmware.common import loopingcall
 from oslo_vmware import exceptions
 from oslo_vmware import vim_util
-from oslo_vmware.common import loopingcall
 
 
 LOG = logging.getLogger(__name__)
