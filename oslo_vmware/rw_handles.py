c3a0b8892cbdc50f6bdf5de92283cea1419800ed
Fabian Wiesel <fabian.wiesel@sap.com>
2022-05-05 14:13:33 +0200

    Accept pre-build cookie for FileRead/WriteHandle
    DatastoreURL.get_transfer_ticket returns already a prebuild
    string, and users need to go through some extra hoops to
    pass a cookiejar compatible struct.
    By accepting the string directly, we simplify the use
    of the two components in oslo_vmware together.
    
    Change-Id: Ia9f16758c388afe0fe05034162f516844ebc6b2b
    (cherry picked from commit 67fd9e50d05ea14e81d3ea23f2be863b449f10f1)

--- oslo_vmware/rw_handles.py
+++ oslo_vmware/rw_handles.py
@@ -30,6 +30,7 @@ from oslo_utils import excutils
 from oslo_utils import netutils
 import requests
 import urllib.parse as urlparse
+
 from urllib3 import connection as httplib
 
 from oslo_vmware._i18n import _
